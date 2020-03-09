6e3107738824982bf0efa9ea78252c7312200098
Galyl3 <61889301+Galyl3@users.noreply.github.com>
2020-03-09 00:28:31 -0600

    Add files via upload

---
+++ ManejadorRaton.java
@@ -0,0 +1,19 @@
+
+package manejadorraton;
+
+ // Demostración de los eventos de ratón.
+  import java.awt.Color;
+  import java.awt.BorderLayout;
+  import java.awt.event.MouseListener;
+  import java.awt.event.MouseMotionListener;
+  import java.awt.event.MouseEvent;
+  import javax.swing.JFrame;
+  import javax.swing.JLabel;
+  import javax.swing.JPanel;
+public class ManejadorRaton {
+   
+public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
